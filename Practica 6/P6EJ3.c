9e922910e12ace734f7a3545509cfb23517e782a
alantangoing <a.clerici@tangoingegneria.com>
2025-11-12 16:06:43 -0300

    P6EJ3

---
+++ Practica 6/P6EJ3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int maximo(float Arr[5]){
+    int pos_max = 0;
+    for(int i = 1; i < 5; i++){
+        if(Arr[i] > Arr[pos_max]){
+            pos_max = i;
+        }
+    }
+    return pos_max;
+}
+
+int main(){
+    float T[5];
+    for(int i = 0; i < 5; i++){
+        printf("Ingrese el valor %d: ", i + 1);
+        scanf("%f", &T[i]);
+    }
+    int posicion = maximo(T);
+    printf("El valor maximo es %.2f y se encuentra en la posicion %d\n", T[posicion], posicion + 1);
+
+    return 0;
+}
