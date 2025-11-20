0351a5f09df35bcb1f69d39575f945d281909b52
alantangoing <a.clerici@tangoingegneria.com>
2025-11-20 10:54:30 -0300

    Agregado ejemplo de escritura de archivos

---
+++ TP2025_2C/ejemplo_escritura.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    FILE *archivo;
+    char texto[10] = "Ejemplo"; // Genero una cadena de texto de ejemplo
+    int i;
+
+    archivo = fopen("archivo_escrito.txt", "w");
+    if (archivo == NULL) {
+        perror("Error al abrir el archivo");
+        return 1;
+    }
+
+    for(i=0;i<10;i++) {
+        fprintf(archivo, "%s %d\n", texto, i);
+        printf("%s %d\n", texto, i);
+    }
+
+    fclose(archivo);
+    return 0;
+}
